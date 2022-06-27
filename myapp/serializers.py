6a6120e2da4ed704cd508dd75723245e61014112
Panoskontos <panos96el@gmail.com>
2022-06-27 15:31:52 +0300

    f

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -6,15 +6,17 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
-        extra_kwargs = {
-            'password':{
-                'write_only':True
-                }
-        }
+    
+    # Making the password dissapear
+    #     extra_kwargs = {
+    #         'password':{
+    #             'write_only':True
+    #             }
+    #     }
 
-    def create(self, validated_data):
-        password = validated_data.pop('password', None)
-        instance = self.Meta.model(**validated_data)
-        instance.save()
-        return instance
+    # def create(self, validated_data):
+    #     validated_data.pop('password', None)
+    #     instance = self.Meta.model(**validated_data)
+    #     instance.save()
+    #     return instance
 
